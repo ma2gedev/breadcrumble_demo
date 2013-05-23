faccb8f0f05610f4e48dd2d98ff85a3e4d2cffd2
Takayuki Matsubara <takayuki.1229@gmail.com>
2013-05-24 00:25:49 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BreadcrumbleApp::Application.config.secret_key_base = '9fef0a023def34ec1349283b6d2fa069fdc415f8cafad66004e683a31f45d57d931022cd9226cdf728c935a8b2f301e7b6fa44475e8c547a2ae5850d3434ac29'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BreadcrumbleApp::Application.config.session_store :cookie_store, key: '_breadcrumble_app_session'
