621b48b16b507209355b5eed12e5bb4df0575d54
Takayuki Matsubara <takayuki.1229@gmail.com>
2013-05-24 00:35:16 +0900

    add controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 BreadcrumbleApp::Application.routes.draw do
+  get "features/index"
+  get "features/show"
+
+  root "features#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
