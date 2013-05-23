ed80ddf234ab4cbd5b9661a14c34a39f9d67547d
Takayuki Matsubara <takayuki.1229@gmail.com>
2013-05-24 01:12:34 +0900

    add sample code about breadcrumb

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source 'https://rubygems.org'
 
 ruby '2.0.0'
 
+gem 'breadcrumble', '~> 3.0.0'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.0.rc1'
 
--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -1,5 +1,9 @@
 class FeaturesController < ApplicationController
+  add_breadcrumb 'breadcrumble', 'https://github.com/ma2gedev/breadcrumble'
+  add_breadcrumb 'top', ->(c) { c.root_path }
+
   def index
+    add_breadcrumb 'index', features_index_path
   end
 
   def show
